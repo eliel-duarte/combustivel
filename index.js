0f63cb7ea63255d194b05a780ca8639726748a56
eliel-duarte <elieletraud@gmail.com>
2023-04-07 15:15:17 -0300

    booststrap

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 app.listen(3000, () => {
